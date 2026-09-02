dc1a569360c8293aedf87bb6461f6ae7327ea91c
João Gabriel Souza <souza.joaojg@gmail.com>
2026-09-01 21:41:32 -0300

    Implementando nova função no código
    Excluindo funções antigas e repetitivas para utilização de única função com argumentos coringas. Atualizando a forma com que recebe-se a resposta de cada tipo de senha

--- Projeto01-norminette_or_die/solution.py
+++ Projeto01-norminette_or_die/solution.py
@@ -21,36 +21,6 @@ def acha_letras(passw: str, lista_caracteres: list) -> int:
             contador_letras += 1
     return(contador_letras)
 
-print(acha_letras(senha, maiusculas))
-'''
-def acha_maiusculas(passw):
-    contador_maiusculas = 0
-    for caracter in passw:
-        if caracter in maiusculas:
-            contador_maiusculas += 1
-    return(contador_maiusculas)
-
-def acha_minusculas(passw):
-    contador_minusculas = 0
-    for caracter in passw:
-        if caracter in minusculas:
-            contador_minusculas += 1
-    return(contador_minusculas)
-
-def acha_numeros(passw):
-    contador_numeros = 0
-    for caracter in passw:
-        if caracter in numeros:
-            contador_numeros += 1
-    return(contador_numeros)
-
-def acha_especiais(passw):
-    contador_especiais = 0
-    for caracter in passw:
-        if caracter in especiais:
-            contador_especiais += 1
-    return(contador_especiais)
-
 def acha_erro(passw):
     for caracter in passw:
         if caracter not in maiusculas and caracter not in minusculas and caracter not in numeros and caracter not in especiais:
@@ -60,14 +30,14 @@ def acha_erro(passw):
 acha_erro(senha)
 if len(senha) == 0:
     print('Senha Vazia!')
-elif len(senha) >= 12 and acha_maiusculas(senha) >= 3 and acha_minusculas(senha) >= 3 and acha_numeros(senha) >= 3 and acha_especiais(senha) >= 3:
+elif len(senha) >= 12 and acha_letras(senha, maiusculas) >= 3 and acha_letras(senha, minusculas) >= 3 and acha_letras(senha, numeros) >= 3 and acha_letras(senha, especiais) >= 3:
     print('Senha Muito Forte!')
-elif len(senha) >= 9 and acha_maiusculas(senha) >= 2 and acha_minusculas(senha) >= 2 and acha_numeros(senha) >= 2 and acha_especiais(senha) >= 2:
+elif len(senha) >= 9 and acha_letras(senha, maiusculas) >= 2 and acha_letras(senha, minusculas) >= 2 and acha_letras(senha, numeros) >= 2 and acha_letras(senha, especiais) >= 2:
     print('Senha Forte!')
-elif len(senha) < 7 or acha_maiusculas(senha) < 1 or acha_minusculas(senha) < 1 or acha_numeros(senha) < 1 or acha_especiais(senha) < 1:
+elif len(senha) < 7:
     print('Senha Fraca!') 
-elif len(senha) < 9 or acha_maiusculas(senha) < 2 or acha_minusculas(senha) < 2 or acha_numeros(senha) < 2 or acha_especiais(senha) < 2:
+elif len(senha) < 9 or acha_letras(senha, maiusculas) < 2 or acha_letras(senha, minusculas) < 2 or acha_letras(senha, numeros) < 2 or acha_letras(senha, especiais) < 2:
     print('Senha Média!')
-'''
+
 
 
